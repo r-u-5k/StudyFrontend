ca5d4580935052ec400fa678b81e061ceed3bff1
Jin <yj999900@gmail.com>
2024-05-03 14:41:43 +0900

    .

---
+++ react_begin/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import Content from './Content';
+
+function App() {
+    return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+    );
+}
+
+export default App;
---
+++ react_begin/src/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <h2>React Footer</h2>
+    </div>
+  );
+}
+
+export default Footer;
