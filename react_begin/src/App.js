e0d8bddc2c64ed168a1402d9cd24e66570ddd093
Jin <yj999900@gmail.com>
2024-05-08 11:05:04 +0900

    .

--- react_begin/src/App.js
+++ react_begin/src/App.js
@@ -4,12 +4,19 @@ import Footer from './layout/Footer';
 import ContentJSX from './component/ContentJSX';
 import ContentEvent from './component/ContentEvent';
 import ContentChild from './component/ContentChild';
+import ContentProps from './component/ContentProps';
 
 function ChildComp() {
     return <div>Child Component</div>;
 }
 
 function App() {
+    let id = 'guard';
+    let name = '김가드';
+    let address = '서울시';
+    let age = 56;
+    let phone = ['010-1234-5678', '010-9999-0000']
+
     return (
         <div className="App">
             <Header />
@@ -18,12 +25,12 @@ function App() {
             <hr />
             <ContentEvent />
             <hr />
-            <Footer />
-            <hr />
             <ContentChild>
                 <ChildComp />
                 {ChildComp()}
             </ContentChild>
+            <ContentProps id={id} name={name} age={age} address={address} gender={'남'} phone={phone} />
+            <Footer />
         </div>
     );
 }
---
+++ react_begin/src/component/ContentProps.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Content.css';
+
+function ContentProps({id, name, address, gender, age, phone}) {
+    return (
+        <div className="content">
+            <h3 style={{ margin: 0, background: 'pink', color: 'white' }}>props</h3>
+            <ul>
+                <li>아이디: {id}</li>
+                <li>이름: {name}</li>
+                <li>주소: {address}</li>
+                <li>성별: {gender}</li>
+                <li>나이: {age}</li>
+                <li>{`${age}세 ${gender}성 ${name}`}</li>
+                <li>전화번호: {phone[1]}</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ContentProps;
